e4606573c8ecc2dcdbe99adc4b31a92c01b63407
Indra Adityawarman <indra2100018060@webmail.uad.ac.id>
2024-10-01 11:42:41 +0700

    [UI] change ui halaman awal

---
+++ resources/views/footer/footer.blade.php
@@ -0,0 +1,93 @@
+<!-- Footer -->
+<style>
+    .bg-dark {
+        background-color: #FAF9F6 !important;
+    }
+</style>
+<footer class="text-center text-lg-start bg-body-tertiary bg-dark text-dark">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between mt-5 border-bottom">
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5 ">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3"></i>Akatsuki
+                    </h6>
+                    <p>
+                        Akatsuki adalah perusahaan start-up berkembang yang mulai merintis. didirikan oleh duwi dan adit sebagai programmer paling hebat wkwkw
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-lg-block">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Products
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Web Sekolah</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Web Portofolio</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Web E-Commerce</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Finlog</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-lg-block">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Tautan
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Pricing</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Settings</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Orders</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Help</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">Kontak Kami</h6>
+                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88 (Duwi)</p>
+                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88 (Adit)</p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4">
+        © 2024 Copyright:
+        <a class="text-reset fw-bold" href="https://mdbootstrap.com/">Akatsuki</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
